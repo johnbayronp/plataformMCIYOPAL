2c1b89a46ed6b01d70c66055ce6d00ef408add00
root <webdeprueba1.1@gmail.com>
2021-02-27 22:32:22 -0500

    deploy --prod test1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig :{
     apiKey: "AIzaSyDVsuEK7FiOvoKEO5sZ596DXYl73gmmjeQ",
     authDomain: "mciyopal-2e032.firebaseapp.com",
