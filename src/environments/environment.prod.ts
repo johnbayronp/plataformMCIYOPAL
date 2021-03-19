b803c5d241a7414b0037275c13d9533c755f61d6
root <webdeprueba1.1@gmail.com>
2021-03-19 15:20:00 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyDVsuEK7FiOvoKEO5sZ596DXYl73gmmjeQ",
+    authDomain: "mciyopal-2e032.firebaseapp.com",
+    projectId: "mciyopal-2e032",
+    storageBucket: "mciyopal-2e032.appspot.com",
+    messagingSenderId: "466332446957",
+    appId: "1:466332446957:web:9035b72c588ace17a1dce4",
+    measurementId: "G-YNX9YYQLHR"
+  },
 };
